49cf077bf46f2c4fcb8837d64db1c7db024ad565
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-11 12:06:30 +0200

    added module files

---
+++ matchms/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Documentation about matchms"""
+
+from .__version__ import __version__
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Netherlands eScience Center"
+__email__ = 'generalization@esciencecenter.nl'
+__all__ = [
+    "__version__"
+]
